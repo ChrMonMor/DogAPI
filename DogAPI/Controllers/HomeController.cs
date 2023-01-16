64998a71044b9ec0c5f94579e2e53d1e4036c345
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2023-01-16 14:55:29 +0100

    Har forsøgt at lære det jeg kan om .net framework's web appication. Jeg har fået en gode hånd på hvordan det er at appilkationen bygger sig selv op og jeg tror at næste gang burde jeg være i stand til at få lavet en model til hundebillederne og deres kategorier, samt lave et mockup af designet

--- DogAPI/Controllers/HomeController.cs
+++ DogAPI/Controllers/HomeController.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace DogAPI.Controllers
 {
+    public class DogBreedList
+    {
+        public int Id { get; set; }
+        public string Breed { get; set; }
+        public string[] Subreeds { get; set; }
+    }
+    public class DogImage
+    {
+        public DogImage(string json)
+        {
+            JObject jObject = o["high"].bJObject.Parse(json);
+            Status = (string)jObject["status"];
+            Message = (string)jObject["message"].ToArray();
+        }
+
+        public string Status { get; set; }
+        public string[] Message { get; set; }
+    }
+
+    public class CallAPI
+    {
+       
+    }
+
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             ViewBag.Message = "Hallo World";
